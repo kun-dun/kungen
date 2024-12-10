c36ac77c54f9df4651ea72039120a0634200da8b
kun-dun <150683132+kun-dun@users.noreply.github.com>
2024-12-10 16:54:37 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      ylalo
+#
+# Created:     10-12-2024
+# Copyright:   (c) ylalo 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from dash import Dash, html,Input,Output,callback
+from dash import dcc
+import dash_cytoscape as cyto
+import os
+import json
+import sys
+import webbrowser
+
+
+
+app = Dash(__name__)
+app= app.server
+
+if __name__ == '__main__':
+   print('Hello world')
+   app.rundebug=True
