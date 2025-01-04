cf88e5b34bc9ca75c50bdabaab420d71f7a668be
kun-dun <150683132+kun-dun@users.noreply.github.com>
2025-01-04 15:28:45 -0300

    Update gen.py

--- gen.py
+++ gen.py
@@ -13677,8 +13677,8 @@ def update_dropdown(data):
     if data is None:
         return [], None
 
-    person_dir = os.path.join(adir, str(data['personneid']))
     person_dir = str(data['personneid'])+'/' #os.path.join(adir, str(data['personneid']))
+    persone_dir=f"person_dir"
     persone_dir=f"person_dir"
     ctypes.windll.user32.MessageBoxW(0, person_dir, "Your title", 1)
     if not os.path.exists(person_dir):
